b040bfef23b66005b25a1ae69fce2fc0e9ffae82
Kevin Shen <kevinshen.kevinshen@gmail.com>
2022-10-18 19:33:58 +0200

    ready for pip

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # Configuration file for the Sphinx documentation builder.
 #
--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setuptools.setup(
     version='0.1.0',
     author='Kevin Shen',
     author_email='kevinshen.abcd@gmail.com',
-    description='vqc_lite: A reader-friendly object-oriented Python implementation of Variational Quantum Circuits '
-                '(VQC) based on Jax and Pennylane',
+    description='vqc_lite: A reader-friendly Python implementation of Variational Quantum Circuits (VQC) based on Jax '
+                'and Pennylane',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/kevinkayyy/VQC-Lite',
